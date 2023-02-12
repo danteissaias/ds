3707bf880ba97710c8976f3254bdd7d13f5c75fd
Dante Issaias <dante@issaias.com>
2023-02-12 22:34:38 +0000

    Fix css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), solidPlugin({ hot: false })],
+  plugins: [tsconfigPaths(), solid()],
 });
