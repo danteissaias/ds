311f99261b3169e5174021f0f09006d5cca059f7
Dante Issaias <dante@issaias.com>
2023-02-15 17:53:37 +0000

    Remove comments

--- packages/ds/src/components/index.ts
+++ packages/ds/src/components/index.ts
@@ -1,5 +1,3 @@
 export * from "./Button/Button";
 export * from "./Card/Card";
 export * from "./Spinner/Spinner";
-// export * as Dropdown from "./Dropdown/Dropdown";
-// export * as TextField from "./TextField/TextField";
