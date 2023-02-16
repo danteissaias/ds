bb1bc8678be106e9e25dfa110c862029d8879b74
Dante Issaias <dante@issaias.com>
2023-02-16 14:32:16 +0000

    Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       output: { assetFileNames: "index.css" },
       external: [
         ...Object.keys(pkg.dependencies),
+        ...Object.keys(pkg.peerDependencies),
         "solid-js",
         "solid-js/web",
         "solid-js/store",
